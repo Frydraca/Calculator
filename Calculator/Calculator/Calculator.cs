978b3b56f2b4119b03edd70348ded2078499e121
Bödő Lajos <ifj.bodo.lajos@gmail.com>
2021-06-04 15:22:42 +0200

    initial commit

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+            this.Text = "Calculator";
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
